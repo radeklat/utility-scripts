def47cae7c47c51b9117044b01e2ae5a73ab64b0
Radek Lát <radek.lat@gmail.com>
2024-03-18 18:32:49 +0100

    feat(show-metered-connection-status): Handle missing ethernet and no connection

--- show_metered_connection_status.py
+++ show_metered_connection_status.py
@@ -3,14 +3,33 @@
 import subprocess
 
 
-def is_not_metered(interface):
-    return subprocess.run(
-        ["nmcli", "-t", "-m", "tabular", "-f", "GENERAL.METERED", "dev", "show", interface],
-        check=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        text=True,
-    ).stdout.strip() == "no"
+def is_metered(interface):
+    try:
+        status = subprocess.run(
+            ["nmcli", "-t", "-m", "tabular", "-f", "GENERAL.METERED", "dev", "show", interface],
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+        ).stdout.strip()
+    except subprocess.CalledProcessError as exc:
+        if f"Device '{interface}' not found" in exc.stderr:
+            return None
+        raise
+
+    if status == "no":
+        return False
+    if status == "yes":
+        return True
+
+    return None
 
 
-print("🌍️" if any(is_not_metered(_) for _ in ["enx32531cd5255a", "wlp0s20f3"]) else "📱")
+statuses = {is_metered(_) for _ in ["enx32531cd5255a", "wlp0s20f3"]}
+
+if statuses == {None}:
+    print("📵")
+elif False in statuses:
+    print("🌍️")
+else:
+    print("📱")
