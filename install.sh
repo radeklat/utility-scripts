8e394a85e9377eb317ed0825329c4f4e4a432a14
Radek Lát <radek.lat@heureka.group>
2024-08-01 16:32:35 +0200

    fix(vpn-autoconnect): Restart service on install

--- install.sh
+++ install.sh
@@ -24,6 +24,7 @@ done
 sudo cp etc/systemd/system/vpn_autoconnect.service  /etc/systemd/system/vpn_autoconnect.service
 sudo systemctl daemon-reload  # if it already exists
 sudo systemctl enable vpn_autoconnect.service
+sudo systemctl stop vpn_autoconnect.service
 sudo systemctl start vpn_autoconnect.service
 
 sudo cp etc/polkit-1/rules.d/50-org.freedesktop.NetworkManager.rules /etc/polkit-1/rules.d/
